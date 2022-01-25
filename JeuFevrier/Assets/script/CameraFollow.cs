c98c051c29440e7c4546cf8574e47bc236b79890
Melinette13 <97893291+Melinette13@users.noreply.github.com>
2022-01-25 11:57:54 +0100

    3

--- JeuFevrier/Assets/script/CameraFollow.cs
+++ JeuFevrier/Assets/script/CameraFollow.cs
@@ -5,10 +5,14 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
 
-    public float smoothSpeed = 0.125f; 
+    public float smoothSpeed = 12f;
 
-    void LateUpdate()
+    public Vector3 offset;
+
+    void FixedUpdate()
     {
-        transform.position = target.position; 
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition; 
     }
 }
